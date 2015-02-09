7834c9abf37e91298064b8dd3e2572b51b8d3325
Iszak Bryan <iszak.bryan@gmail.com>
2015-02-09 23:01:41 +0000

    Fixed vagrant file, install git/bundler

--- provisioners/shell/puppet.sh
+++ provisioners/shell/puppet.sh
@@ -4,9 +4,12 @@ wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 dpkg -i puppetlabs-release-trusty.deb
 
 apt-get update
-apt-get install puppet hiera facter --yes
+apt-get install puppet --yes
+apt-get install hiera --yes
+apt-get install facter --yes
+apt-get install bundler --yes
+apt-get install git --yes
 
 gem install deep_merge --version=1.0.1
 
 rm puppetlabs-release-trusty.deb
-
